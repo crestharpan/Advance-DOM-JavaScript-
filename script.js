37af9d1f27dfcb375129c9f5966c87e07d6424fd
crestharpan <arpanshrestha225@gmail.com>
2024-07-28 19:46:19 +0545

    Implemented a smooth tabbed component

--- script.js
+++ script.js
@@ -58,3 +58,24 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     console.log('nothing');
   }
 });
+//Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  //if clicked happened outside the button the closest element is null so
+
+  if (clicked === null) return;
+  //remove the active class before activing the another button
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  //Active tab
+  clicked.classList.add('operations__tab--active');
+  //Active content area
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
