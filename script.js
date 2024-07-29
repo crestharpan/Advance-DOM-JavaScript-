eb7b6991c8911cdf5fcc9e12b24554b08f768a8e
crestharpan <arpanshrestha225@gmail.com>
2024-07-29 22:13:26 +0545

    image revealing

--- script.js
+++ script.js
@@ -126,3 +126,21 @@ allSections.forEach(section => {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+//Loading image animation
+const imgTargets = document.querySelectorAll(img[data - src]);
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  //Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unObserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+imgTargets.forEach(img => imgObserver.observe(img));
